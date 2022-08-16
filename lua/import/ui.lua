cd3be6736a78aa07357d97efa71e7ab5e542532b
Mike Iversen <miversen33@gmail.com>
2022-08-15 23:13:57 -0500

    Updated time division to be "correct"

--- lua/import/ui.lua
+++ lua/import/ui.lua
@@ -192,7 +192,7 @@ function M._load_info()
     local lines = {}
     table.insert(lines, module)
     table.insert(lines, "    Imported: " .. details.status)
-    table.insert(lines, "    Import Time: " .. details.import_time / 10000 .. " milliseconds")
+    table.insert(lines, "    Import Time: " .. details.import_time / 100000 .. " milliseconds")
     table.insert(lines, "")
     table.insert(lines, "    Errors: ")
     local whitespace = '        '
